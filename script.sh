77f32ab74cb501925656acb44674968180f3ead0
weirdball7 <moteamitay04@gmail.com>
2026-01-14 23:39:03 +0200

    Creatrd CREATE_OUT_DIR function.

--- script.sh
+++ script.sh
@@ -13,8 +13,36 @@ function CHECK_ROOT()
         echo "User is root! ... Proceeding....."
         figlet "ROOT"
         sleep 1
-        CHECK_TOOLS
+        CREATE_OUT_DIR
     fi
+}
+
+function CREATE_OUT_DIR()
+{
+    echo "Please choose the option you prefer..."
+    echo "(1) Create the output directory in the current path."
+    echo "(2) Create the output directory in a path i choose."
+    read CHOICE
+    case $CHOICE in 
+        "1")
+            HOME=$(pwd)
+            mkdir "$HOME/MegaScannerOutput"
+            echo "Output directory created in $HOME/MegaScannerOutput"
+            sleep 1
+            ;;
+        "2")
+            echo "Please provide a path for output directory."
+            read PATH
+            mkdir "$PATH/MegaScannerOutput"
+            echo "Output directory created in $PATH/MegaScannerOutput"
+            sleep 1
+            ;;
+        *) 
+          echo "Invalid option."
+          CREATE_OUT_DIR
+          ;;
+    esac
+    CHECK_TOOLS
 }
 
 CHECK_ROOT
